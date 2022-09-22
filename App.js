69493a131e58113b5cd5950d04c0d7880b7a871a
Kai <kwiddeson4@gmail.com>
2022-09-22 21:59:59 +0100

    add basic structure to the app

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState} from 'react';
+import Header from './components/header';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [todos, setTodos] = useState([
+    {text: 'item1', key: '1'},
+    {text: 'item2', key: '2'},
+    {text: 'item3', key: '3'}
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        {/* to do form */}
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({item}) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +33,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  }
 });
